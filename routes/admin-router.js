3f0d305c3bdf14ca75eab8dd994236992856f650
Muneeb Tahir <muneebtahir@Muneebs-MacBook-Pro.local>
2023-09-29 04:38:30 +0500

    portfolio

--- routes/admin-router.js
+++ routes/admin-router.js
@@ -1,14 +1,13 @@
-const express = require('express')
-const { protect } = require("../middlewares/auth");
+const express = require("express");
 
-const adminCtrl = require('../controllers/admin-ctrl');
+const adminCtrl = require("../controllers/admin-ctrl");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/registerAdmin', adminCtrl.adminSignup)
-router.post('/loginAdmin', adminCtrl.adminLogin)
+router.post("/registerAdmin", adminCtrl.adminSignup);
+router.put("/updateAdmin", adminCtrl.updateAdmin);
+router.post("/loginAdmin", adminCtrl.adminLogin);
+router.get("/portfolio", adminCtrl.AdminPortfolio);
 router.post("/getLoggedInAdmin", adminCtrl.getLoggedInUser);
 
-
-
-module.exports = router
+module.exports = router;
