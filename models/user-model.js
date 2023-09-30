6329b91d36e7478c8ddde3daf6eb3cf02bb5ec44
Muneeb Tahir <muneebtahir@Muneebs-MacBook-Pro.local>
2023-09-30 12:51:45 +0500

    latest

--- models/user-model.js
+++ models/user-model.js
@@ -1,15 +1,6 @@
 var mongoose = require("mongoose");
 var UserSchema = mongoose.Schema({
   fullName: {
-<<<<<<< HEAD
-=======
-    type: String,
-  },
-  address: {
-    type: String,
-  },
-  img: {
->>>>>>> e7778718d3c1966f317c3915cc14f885234b4ff0
     type: String,
   },
   email: {
